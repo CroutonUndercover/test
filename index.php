9af7e309254abb3938648c52a193efddfa2d0856
1309831 <vpmitsev@mail.bg>
2015-10-19 16:04:05 +0100

    added opras  face age if

--- index.php
+++ index.php
@@ -9,7 +9,15 @@
 <img src="oprah.jpg" alt="oprah" width="500" height="500">
 <p>
     <?php
+    $age = 18;
         echo "Im being hunted";
+    if ($age >= 21){
+        print "1 2 3";
+    }
+    elseif($age >= 21){
+        print "1 2";
+    }
+    else print "1";
 
     ?>
 </p>
